4874c62cc506a1df814403b79c74079d6e0d2feb
rageofkurd <rageofkurd98@gmail.comm>
2022-05-30 13:00:29 +0300

    routing to (addBenefit) from (Benefits) page included

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
-import './App.css';
+import "./App.css";
 import { AdminAddEmployerPage } from "./Components/Admin/AddEmployer";
 import { AdminBenefits } from "./Components/Admin/Benefit";
 import { AdminDash } from "./Components/Admin/Dashboard";
@@ -8,14 +8,16 @@ import { AdminEditEmployerPage } from "./Components/Admin/EditEmployer";
 import { AdminEmployeesPage } from "./Components/Admin/Employee";
 import { AdminEmployersPage } from "./Components/Admin/Employer";
 import Login from "./Components/Login";
-import { Signup } from './Components/Signup';
+import { Signup } from "./Components/Signup";
 import { useEffect } from "react";
 import { EmployerBenefitsPage } from "./Components/Employer/Benefit";
 import { AuthenticationPage } from "./Components/BeingAuthenticated";
 import { EmployerBenefitsAssigningPage } from "./Components/Employer/BenefitAssigning";
+import { AdminAddBenefitPage } from "./Components/Admin/AddBenefit";
+import { AdminEditBenefitPage } from "./Components/Admin/EditBenefit";
 
 function App() {
-  let navigate = useNavigate()
+  let navigate = useNavigate();
   let validateurl = "http://localhost:7000/auth/validate";
   let refreshurl = "http://localhost:7000/auth/refreshToken";
 
@@ -24,59 +26,84 @@ function App() {
       if (localStorage.getItem("accessToken") != null) {
         const refreshTokens = async () => {
           try {
-            let response = await fetch(validateurl, { headers: { "Authorization": localStorage.getItem("accessToken") } })
-            let responseinjson = await response.json()
+            let response = await fetch(validateurl, {
+              headers: { Authorization: localStorage.getItem("accessToken") },
+            });
+            let responseinjson = await response.json();
             if (responseinjson.message == "Authorized") {
-              let newtokenresponse = await fetch(refreshurl, { headers: { "Authorization": localStorage.getItem("accessToken") } })
+              let newtokenresponse = await fetch(refreshurl, {
+                headers: { Authorization: localStorage.getItem("accessToken") },
+              });
               let newtokenresponseinjson = await newtokenresponse.json();
-              localStorage.setItem("accessToken", `Bearer ${newtokenresponseinjson.data.token.accessToken}`)
-            }
-            else {
+              localStorage.setItem(
+                "accessToken",
+                `Bearer ${newtokenresponseinjson.data.token.accessToken}`
+              );
+            } else {
               localStorage.removeItem("accessToken");
               localStorage.removeItem("role");
             }
           } catch (err) {
-            console.log(err)
+            console.log(err);
           }
-        }
-        (async () => await refreshTokens())()
+        };
+        (async () => await refreshTokens())();
       }
     }, 14 * 60 * 1000);
 
     setInterval(() => {
       const authentication = async () => {
         if (localStorage.getItem("accessToken") != null) {
-          let response = await fetch(validateurl, { headers: { "Authorization": localStorage.getItem("accessToken") } });
-          let responseinjson = await response.json()
+          let response = await fetch(validateurl, {
+            headers: { Authorization: localStorage.getItem("accessToken") },
+          });
+          let responseinjson = await response.json();
           if (responseinjson.message != "Authorized") {
             signout();
           }
         }
-      }
+      };
       (async () => await authentication())();
     }, 5000);
-  })
+  });
 
   const signout = () => {
-    localStorage.clear()
-    navigate("/login")
-  }
+    localStorage.clear();
+    navigate("/login");
+  };
 
   return (
     <Routes>
       if(tr)
-      <Route path='login' element={<Login />}></Route>
-      <Route path='signup' element={<Signup />}></Route>
-      <Route path='admin/dashboard' element={<AdminDash />}></Route>
-      <Route path='admin/benefits' element={<AdminBenefits />}></Route>
-      <Route path='admin/employees' element={<AdminEmployeesPage />}></Route>
-      <Route path='admin/companies' element={<AdminEmployersPage />}></Route>
-      <Route path='admin/companies/add' element={<AdminAddEmployerPage />}></Route>
-      <Route path='admin/companies/:id' element={<AdminEditEmployerPage />}></Route>
-      <Route path='employer/dashboard' element={<EmployerDash />}></Route>
-      <Route path='employer/benefits' element={<EmployerBenefitsAssigningPage />}></Route>
-      <Route path='authentication' element={<AuthenticationPage />}></Route>
-      <Route path='*' element={<Navigate to="/login" />}></Route>
+      <Route path="login" element={<Login />}></Route>
+      <Route path="signup" element={<Signup />}></Route>
+      <Route path="admin/dashboard" element={<AdminDash />}></Route>
+      <Route path="admin/benefits" element={<AdminBenefits />}></Route>
+      <Route path="admin/employees" element={<AdminEmployeesPage />}></Route>
+      <Route
+        path="admin/benefits/add"
+        element={<AdminAddBenefitPage />}
+      ></Route>
+      <Route
+        path="admin/benefits/edit"
+        element={<AdminEditBenefitPage />}
+      ></Route>
+      <Route path="admin/companies" element={<AdminEmployersPage />}></Route>
+      <Route
+        path="admin/companies/add"
+        element={<AdminAddEmployerPage />}
+      ></Route>
+      <Route
+        path="admin/companies/:id"
+        element={<AdminEditEmployerPage />}
+      ></Route>
+      <Route path="employer/dashboard" element={<EmployerDash />}></Route>
+      <Route
+        path="employer/benefits"
+        element={<EmployerBenefitsAssigningPage />}
+      ></Route>
+      <Route path="authentication" element={<AuthenticationPage />}></Route>
+      <Route path="*" element={<Navigate to="/login" />}></Route>
     </Routes>
   );
 }
