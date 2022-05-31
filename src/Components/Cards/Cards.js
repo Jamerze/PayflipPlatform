c928bc8966bdbaae45f4294507dd5b6bae7507ff
rageofkurd <rageofkurd98@gmail.comm>
2022-06-01 02:32:08 +0300

    Hevar Added (hard coded) Card Component to the Benefits Page of Employeer

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -7,23 +7,52 @@ import {
   MDBCardImage,
   MDBBtn,
 } from "mdb-react-ui-kit";
-
+import { Container, Row, Col } from "reactstrap";
 export const BenefitCards = () => {
-  return <Card></Card>;
+  return (
+    <Container>
+      <Row md={3}>
+        <Col>
+          <Card></Card>
+        </Col>
+        <Col>
+          {" "}
+          <Card></Card>
+        </Col>
+        <Col>
+          <Card></Card>
+        </Col>
+      </Row>
+      <br></br>
+    </Container>
+  );
 };
 
 const Card = () => {
   return (
-    <MDBCard style={{ maxWidth: "15rem", maxHeight: "21rem" }}>
+    <MDBCard
+      alignment="center"
+      style={{ maxWidth: "15rem", maxHeight: "21rem" }}
+    >
       <MDBCardImage
         src="https://i.pinimg.com/originals/86/09/64/860964688d449da06d61c6cc4e9b931a.jpg"
         position="top"
         alt="..."
       />
       <MDBCardBody>
-        <MDBCardTitle>Playstation</MDBCardTitle>
+        <MDBCardTitle>
+          Playstation{" "}
+          {<span style={{ fontSize: 15, color: "green" }}>$200</span>}
+        </MDBCardTitle>
+
         <MDBCardText>full pack gaming console</MDBCardText>
-        <MDBBtn href="#">purchase</MDBBtn>
+        <button
+          type="submit"
+          className="btn bg-redpayflip text-white w-80 mt-1 mb-0"
+          disabled={false}
+        >
+          Purchase
+        </button>
       </MDBCardBody>
     </MDBCard>
   );
